9b211decb2ddb4c7b915a4439f1f672d6e7be941
Benjamen Alford <benjamen.alford@gmail.com>
2023-09-25 20:52:51 -0500

    full class example

--- In Class/10-Advanced-SQL/3/Activities/06-Ins_Jsonify/Unsolved/app.py
+++ In Class/10-Advanced-SQL/3/Activities/06-Ins_Jsonify/Unsolved/app.py
@@ -1,4 +1,9 @@
 from flask import Flask, jsonify
+# Python SQL toolkit and Object Relational Mapper
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, inspect
 
 app = Flask(__name__)
 
@@ -13,10 +18,92 @@ justice_league_members = [
     {"superhero": "Wonder Woman", "real_name": "Princess Diana"}
 ]
 
+engine = create_engine("sqlite:///../../mammal_masses.sqlite", echo=False)
+# open DB and reflect data
+# Reflect Database into ORM classes
+Base = automap_base()
+Base.prepare(autoload_with=engine)
+NA = Base.classes.na
+
 
 @app.route('/')
 def home():
     return jsonify(["super_hero"])
+
+
+@app.route("/api/mammal")
+def hoobstank():
+    # create the session
+    session = Session(engine)
+    # read the data
+    data = session.query(NA.species).all()
+    species = []
+    # transform the data
+    for item in data:
+        species.append(item[0])
+    # return the data
+    return jsonify(species)
+
+
+@app.route("/api/mammal/<some_species>")
+def hoobstank2(some_species):
+    # create the session
+    session = Session(engine)
+    # read the data
+    data = session.query(NA).filter(NA.species == some_species).all()
+    species = []
+    # transform the data
+    for item in data:
+        animal = {}
+        animal["species"] = item.species
+        animal["status"] = item.status
+        animal["order"] = item.order
+        animal["family"] = item.family
+        animal["genus"] = item.genus
+        species.append(animal)
+    # return the data
+    return jsonify(species)
+
+
+@app.route("/api/mammal/status/<status>")
+def hoobstank3(status):
+    # create the session
+    session = Session(engine)
+    # read the data
+    data = session.query(NA).filter(NA.status == status).all()
+    species = []
+    # transform the data
+    for item in data:
+        animal = {}
+        animal["species"] = item.species
+        animal["status"] = item.status
+        animal["order"] = item.order
+        animal["family"] = item.family
+        animal["genus"] = item.genus
+        species.append(animal)
+    # return the data
+    return jsonify(species)
+
+
+@app.route("/api/mammal/status/<status>/<some_species>")
+def hoobstank4(status, some_species):
+    # create the session
+    session = Session(engine)
+    # read the data
+    data = session.query(NA).filter(NA.status == status).filter(
+        NA.species == some_species).all()
+    species = []
+    # transform the data
+    for item in data:
+        animal = {}
+        animal["species"] = item.species
+        animal["status"] = item.status
+        animal["order"] = item.order
+        animal["family"] = item.family
+        animal["genus"] = item.genus
+        species.append(animal)
+    # return the data
+    return jsonify(species)
 
 
 @app.route("/api")
