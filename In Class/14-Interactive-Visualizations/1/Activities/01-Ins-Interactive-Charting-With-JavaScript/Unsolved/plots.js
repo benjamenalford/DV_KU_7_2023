7544710dd7d8daed2a730a2e055dbbb017833428
Benjamen Alford <benjamen.alford@gmail.com>
2023-10-23 18:55:56 -0500

    plot example

--- In Class/14-Interactive-Visualizations/1/Activities/01-Ins-Interactive-Charting-With-JavaScript/Unsolved/plots.js
+++ In Class/14-Interactive-Visualizations/1/Activities/01-Ins-Interactive-Charting-With-JavaScript/Unsolved/plots.js
@@ -1,1 +1,17 @@
 // External JavaScript
+console.log("Hello wolrd");
+var name = "ben";
+var age = 42;
+console.log("I am " + name + " and am " + age)
+console.log(`I am ${name} and am ${age}`)
+
+
+var trace1 = {
+    x: [0, 1, 2, 3],
+    y: [10, 20, 30, 40],
+    type: 'bar'
+}
+var layout = {
+    title: name
+}
+Plotly.newPlot("plot", [trace1], layout)
