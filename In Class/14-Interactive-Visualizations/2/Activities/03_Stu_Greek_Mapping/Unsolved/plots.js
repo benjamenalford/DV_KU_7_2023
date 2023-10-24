66f0b871fe8117fd62ab7d40f0a7d415e01ce8eb
Benjamen Alford <benjamen.alford@gmail.com>
2023-10-23 21:12:29 -0500

    map is awesome

--- In Class/14-Interactive-Visualizations/2/Activities/03_Stu_Greek_Mapping/Unsolved/plots.js
+++ In Class/14-Interactive-Visualizations/2/Activities/03_Stu_Greek_Mapping/Unsolved/plots.js
@@ -1,12 +1,15 @@
 console.log(data);
 
 // Trace for the Greek Data
-
-
-// Data trace array
+trace1 = {
+    x: data.map(god => god.greekName),
+    y: data.map(god => god.greekSearchResults),
+    type: 'bar'
+}
 
 
 // Apply the group barmode to the layout
 
 
 // Render the plot to the div tag with id "plot"
+Plotly.newPlot("plot", [trace1])
