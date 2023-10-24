2712a91cfa71aeaac3e40d4c383de6fe39a9067a
Benjamen Alford <benjamen.alford@gmail.com>
2023-10-23 20:27:57 -0500

    part 1

--- In Class/14-Interactive-Visualizations/1/Activities/08-Stu_Multiple-Trace-Charts/Unsolved/plots.js
+++ In Class/14-Interactive-Visualizations/1/Activities/08-Stu_Multiple-Trace-Charts/Unsolved/plots.js
@@ -9,18 +9,39 @@ let romanSearchResults = []
 
 // YOUR CODE HERE
 // For loop to populate arrays
-
+for (let i = 0; i < searchResults.length; i++) {
+    let god = searchResults[i];
+    greekNames.push(god.greekName)
+    romanNames.push(god.romanName)
+    names.push(god.pair)
+    greekSearchResults.push(god.greekSearchResults)
+    romanSearchResults.push(god.romanSearchResults)
+}
 
 // Trace1 for the Greek Data
-
+let greekTrace = {
+    x: greekNames,
+    y: greekSearchResults,
+    type: 'bar',
+    name: "Greek",
+    text: greekNames
+}
 
 // Trace 2 for the Roman Data
+let romanTrace = {
+    x: romanNames,
+    y: romanSearchResults,
+    type: 'bar',
+    name: "roman",
+    text: romanNames
+}
 
-
+let layout = {
+    title: "greek and roman search results",
+    barmode: "group"
+}
 // Create data array
-
-
-// Apply a title to the layout
-
+let traceData = [greekTrace, romanTrace]
 
 // Render the plot to the div tag with id "plot"
+Plotly.newPlot("plot", traceData, layout)
