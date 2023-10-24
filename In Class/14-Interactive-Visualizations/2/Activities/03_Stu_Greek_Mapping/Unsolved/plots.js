575747d0cb5f68d5392f32f1321bd13c873dd26c
Benjamen Alford <benjamen.alford@gmail.com>
2023-10-24 18:47:09 -0500

    showing filter

--- In Class/14-Interactive-Visualizations/2/Activities/03_Stu_Greek_Mapping/Unsolved/plots.js
+++ In Class/14-Interactive-Visualizations/2/Activities/03_Stu_Greek_Mapping/Unsolved/plots.js
@@ -1,15 +1,22 @@
 console.log(data);
 
+console.log(data.length)
+let newGods = data.filter(function (god) {
+    if (god.greekSearchResults > 1000000 && (god.greekName != 'Nike' && god.greekName != 'Pan'))
+        return god;
+
+});
+let newGods2 = data.filter(god => god.greekSearchResults > 1000000 &&
+    (god.greekName != 'Nike' && god.greekName != 'Pan'));
+
+console.log(newGods.length)
+console.log(newGods2.length)
 // Trace for the Greek Data
 trace1 = {
-    x: data.map(god => god.greekName),
-    y: data.map(god => god.greekSearchResults),
+    x: newGods.map(god => god.greekName),
+    y: newGods.map(god => god.greekSearchResults),
     type: 'bar'
 }
-
-
-// Apply the group barmode to the layout
-
 
 // Render the plot to the div tag with id "plot"
 Plotly.newPlot("plot", [trace1])
