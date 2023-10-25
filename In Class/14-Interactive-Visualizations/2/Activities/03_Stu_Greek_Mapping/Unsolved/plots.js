20bc334bb54106241aa06ff6a3996afc58ecf17e
Benjamen Alford <benjamen.alford@gmail.com>
2023-10-24 19:09:55 -0500

    slice, filter, sort

--- In Class/14-Interactive-Visualizations/2/Activities/03_Stu_Greek_Mapping/Unsolved/plots.js
+++ In Class/14-Interactive-Visualizations/2/Activities/03_Stu_Greek_Mapping/Unsolved/plots.js
@@ -1,22 +1,17 @@
 console.log(data);
 
-console.log(data.length)
-let newGods = data.filter(function (god) {
-    if (god.greekSearchResults > 1000000 && (god.greekName != 'Nike' && god.greekName != 'Pan'))
-        return god;
+let newGods = data.filter(god => god.greekSearchResults > 10000000);
 
-});
-let newGods2 = data.filter(god => god.greekSearchResults > 1000000 &&
-    (god.greekName != 'Nike' && god.greekName != 'Pan'));
+newGods.sort((first, second) => second.greekSearchResults - first.greekSearchResults)
 
-console.log(newGods.length)
-console.log(newGods2.length)
+//newGods = newGods.slice(0, 10)
 // Trace for the Greek Data
 trace1 = {
-    x: newGods.map(god => god.greekName),
-    y: newGods.map(god => god.greekSearchResults),
+    x: newGods.map(god => god.greekName).slice(0, 10),
+    y: newGods.map(god => god.greekSearchResults).slice(0, 10),
     type: 'bar'
 }
 
 // Render the plot to the div tag with id "plot"
 Plotly.newPlot("plot", [trace1])
+
--- In Class/14-Interactive-Visualizations/2/Activities/08-Stu_Sort_and_Slice/Unsolved/sliceSort.js
+++ In Class/14-Interactive-Visualizations/2/Activities/08-Stu_Sort_and_Slice/Unsolved/sliceSort.js
@@ -2,9 +2,18 @@
 let numArray = [9.9, 6.1, 17.1, 22.7, 4.6, 8.7, 7.2];
 
 // Sort the array in descending order and assign the results to a variable
+// numArray.sort() -- JS is weird
+numArray.sort((first, second) => {
+    console.log(first, second, numArray)
+    return second - first
+})
+
+
+
 
 
 // Reverse the array order
 
 
 // Slice the first five elements of the sortedAscending array, assign to a letiable
+numArray.slice(0, 5)
