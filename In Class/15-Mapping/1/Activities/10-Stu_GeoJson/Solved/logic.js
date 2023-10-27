0e2ab5aa0b178f8bf1a488d8514c7769530c9dda
Benjamen Alford <benjamen.alford@gmail.com>
2023-10-26 20:52:40 -0500

    class examples

--- In Class/15-Mapping/1/Activities/10-Stu_GeoJson/Solved/logic.js
+++ In Class/15-Mapping/1/Activities/10-Stu_GeoJson/Solved/logic.js
@@ -1,5 +1,5 @@
 // Store our API endpoint as queryUrl.
-var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2021-01-01&endtime=2021-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
+var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2021-01-01&endtime=2021-01-02";
 
 // Perform a GET request to the query URL/
 d3.json(queryUrl).then(function (data) {
