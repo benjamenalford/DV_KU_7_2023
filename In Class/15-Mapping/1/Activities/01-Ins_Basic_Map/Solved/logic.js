e8d14322f93c59513393b474b1e198b00a710d61
Benjamen Alford <benjamen.alford@gmail.com>
2023-10-26 19:03:14 -0500

    KU

--- In Class/15-Mapping/1/Activities/01-Ins_Basic_Map/Solved/logic.js
+++ In Class/15-Mapping/1/Activities/01-Ins_Basic_Map/Solved/logic.js
@@ -2,12 +2,20 @@
 // We set the longitude, latitude, and starting zoom level.
 // This gets inserted into the div with an id of "map".
 var myMap = L.map("map", {
-  center: [45.52, -122.67],
+  center: [38.960213, -95.277390],
   zoom: 13
 });
 
 // Adding a tile layer (the background map image) to our map:
 // We use the addTo() method to add objects to our map.
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(myMap);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(myMap);
+
+let marker = L.marker([38.960213, -95.277390]).addTo(myMap)
+marker.bindPopup('<h2>Hello There</h2><p>I havent heard that name in a long time</p>')
+  .openPopup();
+
+
+let x = L.marker([38.960513, -95.279390], {
+  title: "I am hooba",
+  draggable: true
+}).addTo(myMap)
