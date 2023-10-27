a1639f36e42fa94d9fddd8fefe04acfcafce9d0e
Benjamen Alford <benjamen.alford@gmail.com>
2023-10-26 19:13:52 -0500

    city markers

--- In Class/15-Mapping/1/Activities/03-Stu_City_Markers/Unsolved/logic.js
+++ In Class/15-Mapping/1/Activities/03-Stu_City_Markers/Unsolved/logic.js
@@ -6,15 +6,40 @@ var myMap = L.map("map", {
 
 // Add a tile layer.
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
 // City markers
 
 // Add code to create a marker for each of the following cities and to add it to the map.
+let cities = [{
+  location: [40.7128, -74.0059],
+  name: "New York",
+  population: 8550405
+},
+{
+  location: [41.8781, -87.6298],
+  name: "Chicago",
+  population: 2720546
+},
+{
+  location: [29.7604, -95.3698],
+  name: "Houston",
+  population: 2296224
+},
+{
+  location: [34.0522, -118.2437],
+  name: "Los Angeles",
+  population: 3971883
+},
+{
+  location: [41.2524, -95.9980],
+  name: "Omaha",
+  population: 446599
+}
+];
 
-// newyork;
-// chicago;
-// houston;
-// la;
-// omaha;
+cities.forEach(city => {
+  L.marker(city.location).addTo(myMap)
+    .bindPopup(`<h3>${city.name}</h3><p>Population: ${city.population}</p>`)
+})
