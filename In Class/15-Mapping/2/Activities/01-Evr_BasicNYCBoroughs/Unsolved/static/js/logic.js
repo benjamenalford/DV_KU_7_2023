916d4e367e66ddbfa4942e2a810a4e8a11e7881e
Benjamen Alford <benjamen.alford@gmail.com>
2023-10-30 19:25:31 -0500

    zoomy zooms

--- In Class/15-Mapping/2/Activities/01-Evr_BasicNYCBoroughs/Unsolved/static/js/logic.js
+++ In Class/15-Mapping/2/Activities/01-Evr_BasicNYCBoroughs/Unsolved/static/js/logic.js
@@ -12,7 +12,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // Use this link to get the GeoJSON data.
 // var link = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/15-Mapping-Web/nyc.geojson";
 let link = "static/data/nyc.geojson"
-
+let zoomed = false
 // Getting our GeoJSON data
 d3.json(link).then(function (data) {
     // Creating a GeoJSON layer with the retrieved data
@@ -32,7 +32,16 @@ d3.json(link).then(function (data) {
 
             //
             layer.on({
-                click: event => myMap.fitBounds(event.target.getBounds()),
+                click: event => {
+                    if (!zoomed) {
+                        myMap.fitBounds(event.target.getBounds())
+                        zoomed = true
+                    }
+                    else {
+                        zoomed = false
+                        myMap.setZoom(11)
+                    }
+                },
                 mouseover: event => {
                     let currentLayer = event.target;
                     currentLayer.setStyle({ fillOpacity: .9 })
