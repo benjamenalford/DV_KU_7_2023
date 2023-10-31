2dca2c09e623590c90a331bdf305804ed1181c1d
Benjamen Alford <benjamen.alford@gmail.com>
2023-10-30 19:13:27 -0500

    almost done

--- In Class/15-Mapping/2/Activities/01-Evr_BasicNYCBoroughs/Unsolved/static/js/logic.js
+++ In Class/15-Mapping/2/Activities/01-Evr_BasicNYCBoroughs/Unsolved/static/js/logic.js
@@ -10,10 +10,39 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(myMap);
 
 // Use this link to get the GeoJSON data.
-var link = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/15-Mapping-Web/nyc.geojson";
+// var link = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/15-Mapping-Web/nyc.geojson";
+let link = "static/data/nyc.geojson"
 
 // Getting our GeoJSON data
 d3.json(link).then(function (data) {
     // Creating a GeoJSON layer with the retrieved data
-    L.geoJson(data).addTo(myMap);
+    L.geoJson(data, {
+        style: (feature) => {
+            return {
+                color: "white",
+                fillColor: colorBorough(feature.properties.borough),
+                fillOpacity: .5,
+                weight: 1
+            }
+        },
+        onEachFeature: function (feature, layer) {
+            let hood = feature.properties.neighborhood;
+            let borough = feature.properties.borough
+            layer.bindPopup(`<h3>${borough}</h3><p>${hood}</p>`)
+
+            //
+            layer.on("click", function (event) {
+                myMap.fitBounds(event.target.getBounds())
+            })
+        }
+    }).addTo(myMap);
 });
+
+function colorBorough(borough) {
+    if (borough == "Bronx") return "yellow"
+    else if (borough == "Manhattan") return "pink"
+    else if (borough == "Queens") return "red"
+    else if (borough == "Brooklyn") return "blue"
+    else if (borough == "Staten Island") return "purple"
+    else return "black"
+}
