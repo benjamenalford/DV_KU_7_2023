c3a6fe70b79f7ce3752541b3067ed03ce06665c9
Benjamen Alford <benjamen.alford@gmail.com>
2023-10-30 19:41:20 -0500

    hooligans

--- In Class/15-Mapping/2/Activities/02-Evr_CrimeHeatmap/Solved/static/js/heatmap.js
+++ In Class/15-Mapping/2/Activities/02-Evr_CrimeHeatmap/Solved/static/js/heatmap.js
@@ -5,15 +5,15 @@ var myMap = L.map("map", {
 
 // Adding the tile layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-var url = "https://data.sfgov.org/resource/cuks-n6tp.json?$limit=10000";
+var url = "https://data.sfgov.org/resource/cuks-n6tp.json?$limit=1000";
 
-d3.json(url).then(function(response) {
+d3.json(url).then(function (response) {
 
   console.log(response);
-
+  response.filter(location => location.category = "VANDALISM")
   var heatArray = [];
 
   for (var i = 0; i < response.length; i++) {
@@ -21,7 +21,9 @@ d3.json(url).then(function(response) {
 
     if (location) {
       heatArray.push([location.coordinates[1], location.coordinates[0]]);
+
     }
+
   }
 
   var heat = L.heatLayer(heatArray, {
